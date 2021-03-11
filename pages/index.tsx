4160d7061e16d1d4161f8462e935353abe53c5f1
Collin Schaafsma <schaafsma@gmail.com>
2021-03-10 21:32:44 -0700

    wip: get stonks

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,66 +1,28 @@
-import { GetStaticProps, NextPage } from 'next'
+import { GetStaticProps } from 'next'
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import { pushShift, Stonk } from '../utils/pushShift'
 
-const Home: NextPage = () => {
+const Home = (props: {stonks: Stonk[]}) => {
+
+  
+  console.log(props.stonks)
+
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Stonks</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+      {props.stonks ? (
+          <div>
+            {props.stonks.map((stonk: Stonk) => (
+              <p>{stonk.title}</p>
+            ))}
+          </div>
+        ) : (
+          <div className="loader">loading...</div>
+        )}
     </div>
   )
 }
@@ -68,9 +30,12 @@ const Home: NextPage = () => {
 export default Home
 
 export const getStaticProps: GetStaticProps = async () => {
+  const stonks = await pushShift<Stonk[]>(
+    `https://api.pushshift.io/reddit/submission/search?size=100&subreddit=wallstreetbets`
+  )
   return {
     props: {
-
+      stonks
     }
   }
 }
---
+++ utils/pushShift.ts
@@ -0,0 +1,12 @@
+import fetch from 'node-fetch'
+
+export async function pushShift<T>(request: RequestInfo): Promise<T> {
+  const response = await fetch(request)
+  const body = await response.json()
+  return body.data
+}
+
+export interface Stonk {
+  title: string
+}
+
